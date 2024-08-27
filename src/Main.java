4716bbdf1547915ff1505d5546887fdf35b83c37
Sofia Quitian <you@example.com>
2024-08-27 11:51:59 -0500

    Opciones de las operasciones

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args){
+        //OPERACIONES ARITMETICAS
+
+        int op;
+        System.out.println("Ingrese una opcion del 1 al 5");
+        System.out.println("\n1-Suma");
+        System.out.println("\n2-Resta");
+        System.out.println("\n3-Multiplicacion");
+        System.out.println("\n4-Division");
+        System.out.println("\n5-Salir");
+
+        Scanner teclado = new Scanner(System.in);
+        op = teclado.nextInt();
+        }
+    }
